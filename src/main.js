bc4fb7d643e4903100f74dc26eb8caa5647e49a7
YanaCondrea <160020879+YanaCondrea@users.noreply.github.com>
2024-07-06 01:49:30 +0100

    update-loader

--- src/main.js
+++ src/main.js
@@ -11,8 +11,6 @@ const key = '44769616-4ffe0cee5617f53d3e1075857';
 
 
 
-
-
 const elements = {
     form: document.querySelector('.search-form'),
     list: document.querySelector('.list'),
@@ -20,30 +18,35 @@ const elements = {
 }
 
 
-
 elements.form.addEventListener('submit', searchQuery);
 
+
 function searchQuery(evt) {
+
     evt.preventDefault();
-     elements.list.innerHTML = '';
+
+    elements.list.innerHTML = '';
+    
     const searchText = evt.target.elements.input.value.trim();
     
     if (searchText !== "") {
+
         elements.form.insertAdjacentHTML("afterend", ' <span class="loader"></span>');
+        const loader = document.querySelector('.loader')
+
         httpRequest(key, searchText)
-          .then(res => {
+    .then(res => {
         if (!res.ok) {
           throw new Error(res.status);
         }
         return res.json();
           })
-         .then((data) => {
-             const image = data.hits;
-             if (image.length !== 0) {
+    .then((data) => {
+        const image = data.hits;
+         if (image.length !== 0) {
                  
     elements.list.insertAdjacentHTML("beforeend", createMurkup(image));
      let lightbox = new SimpleLightbox('.list a', {
-    
 	captions: true,
 	captionType: 'attr',
 	captionsData: 'alt',
@@ -68,7 +71,9 @@ function searchQuery(evt) {
         }); 
              }
          })
-            .catch((err) => console.error("Fetch Error:", err));
+            .catch((err) => console.error("Fetch Error:", err))
+           .finally(() => (loader.style.display = 'none'));
+    elements.form.reset();
         
     } else {
         iziToast.show({
@@ -85,6 +90,7 @@ function searchQuery(evt) {
             messageSize: '16px',
             message: 'Form field must be filled in'
         });
+      
     }   
     
   
